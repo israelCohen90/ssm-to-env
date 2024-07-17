c58ccf9988edb017c9c06547d99ee7a9adfd112a
israel.c@lsports.eu <94558608+israelCohen90@users.noreply.github.com>
2024-07-17 23:54:18 +0300

    create env file from ssm

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+const AWS = require("aws-sdk");
+const fs = require("fs");
+AWS.config.update({ region: "eu-west-1" });
+const ssm = new AWS.SSM();
+
+async function extractParametersFromSSM(paths: any[]) {
+  try {
+    const arr = [];
+    for (const item of paths) {
+      if (!item) continue;
+
+      const result = await ssm
+        .getParameters({
+          Names: [item.path],
+        })
+        .promise();
+      const values = result.Parameters.map((parameter: any) => parameter.Value);
+      console.log(`extract: ${item.name}`);
+      arr.push({ name: item.name, value: values[0] });
+    }
+    return arr;
+  } catch (error) {
+    console.error("Error retrieving parameters:", error);
+    throw error;
+  }
+}
+
+const envFile = fs.readFileSync("env", "utf8");
+const list = envFile.split("\n").map((line: string) => {
+  const sp = line.split("=");
+  const name = sp[0];
+  const path = sp[1];
+  if (name)
+    return {
+      name,
+      path,
+    };
+});
+
+extractParametersFromSSM(list)
+  .then((values) => {
+    fs.writeFileSync(
+      ".env",
+      values.map((item) => `${item.name}=${item.value}`).join("\n")
+    );
+    console.log("saved .env file");
+  })
+  .catch((error) => {
+    console.error("error: ", error);
+  });
